417c3680f09dac4f02851ea53f63f5a44c712422
Dhurv Garg <dhurv@flurriapp.com>
2023-02-06 09:49:25 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Dhurv-Garg`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
